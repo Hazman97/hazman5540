1d1d86d51a0037cc859f9c40c376f37eff2b5604
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-08-29 17:30:10 +0800

    feat: Update about page with improved skill stacks display

--- src/views/portfolio/about.vue
+++ src/views/portfolio/about.vue
@@ -26,13 +26,55 @@
         </div>
       </div>
       <div class="rounded-3xl bg-slate-200 py-2 px-2 dark:bg-blue-900/10 ">
-        <h1 class="capitalize text-xl font-semibold text-teal-500 "> my skill</h1>
+        <h1 class="capitalize text-xl font-semibold text-teal-500 "> Skill</h1>
         <P class="capitalize text-lg font-semibold dark:text-[#ccd6f6] mt-5">stacks used</P>
+        
+        <div class="grid md:grid-cols-4 mt-3 sm:grid-col-1 gap-3">
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3 ">
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">TYPESCRIPT</P>
+            <p class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">HTML (<span class="text-teal-500">intermediate</span>)</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">JAVASCRIPT</P>
+          </div>
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
+            <p class="hover:font-sans hover:text-slate-600  dark:hover:text-teal-100 ">VUE JS (<span class="text-teal-500">intermediate</span>)</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">TAILWINDS (<span class="text-teal-500">intermediate</span>)</P>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">JAVASCRIPT (<span class="text-teal-500">basic</span>)</P>
+          </div>
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
+            <p class="hover:font-sans hover:text-slate-600  dark:hover:text-teal-100 ">PHP (<span class="text-teal-500">basic</span>)</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">MySQL (<span class="text-teal-500">basic</span>)</P>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100"></P>
+          </div>
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
+            <p class="hover:font-sans hover:text-slate-600  dark:hover:text-teal-100 ">Nginx (<span class="text-teal-500">basic</span>)</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">IIS (<span class="text-teal-500">basic</span>)</P>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100"> Cloudflare Tunnel (<span class="text-teal-500">basic</span>)</P>
+          </div>
+        </div>
+        <P class="capitalize text-lg font-semibold dark:text-[#ccd6f6] mt-5">software</P>
         <div class="grid md:grid-cols-3 mt-3 sm:grid-col-1 gap-3">
           <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
-            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">TYPESCRIPT</P>
-            <p class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">HTML & CSS</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">Photoshop (<span class="text-teal-500">basic</span>)</P>
+            <p class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">Premier Pro (<span class="text-teal-500">basic</span>)</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">Capcut (<span class="text-teal-500">basic</span>)</P>
+          </div>
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
+            <p class="hover:font-sans hover:text-slate-600  dark:hover:text-teal-100 ">VUE JS</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">TAILWINDS</P>
             <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">JAVASCRIPT</P>
+          </div>
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
+            <p class="hover:font-sans hover:text-slate-600  dark:hover:text-teal-100 ">PHP</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">MySQL</P>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100"></P>
+          </div>
+        </div>
+        <P class="capitalize text-lg font-semibold dark:text-[#ccd6f6] mt-5">Technical Skills</P>
+        <div class="grid md:grid-cols-3 mt-3 sm:grid-col-1 gap-3">
+          <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">Repair/Custom PC</P>
+            <p class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100">Setup router</p>
+            <P class="hover:font-sans hover:text-slate-600 dark:hover:text-teal-100"> </P>
           </div>
           <div class="text-slate-500 dark:text-white flex flex-col text-xs font-mono  gap-3">
             <p class="hover:font-sans hover:text-slate-600  dark:hover:text-teal-100 ">VUE JS</p>
