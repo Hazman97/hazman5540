867dd76d558025392cf77fad5ca7a1d36608b99e
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2026-05-10 14:49:19 +0800

    chore: remove Vite configuration, Tailwind CSS setup, and project boilerplate files

--- src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <router-view name="nav"></router-view>
-  <router-view name="topBar"></router-view>
-  <router-view/>
-</template>
-
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-
-import './assets/input.css'
-
-createApp(App).use(store).use(router).mount('#app')
